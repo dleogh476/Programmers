a2e91facd1c31c462872032a2f6209b9d584eb24
juwon <67044438+dleogh476@users.noreply.github.com>
2023-05-05 02:09:51 +0900

    [level 2] Title: 땅따먹기, Time: 20.06 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -14,3 +14,42 @@ class Solution {
     }
     
 }
+
+/*
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        //DFS
+        //1. 첫번째 행은 모든 요소들의 모두 DFS함
+        //2. 가장 큰 값을 찾기 위해서 나온 모든 값 비교
+        int val = 0;        
+        for(int idx = 0; idx < 4; idx++){
+            val = DFS(land, 0, idx,land.length);
+            //System.out.println(land.length);
+            if(val > answer){
+                answer = val;
+            }
+        }
+        return answer;
+    }
+    
+    int DFS(int[][] land, int row, int col, int land_row_size){
+        int ret = land[row][col];
+        //맨 마지막 행일 경우 선택한 위치 결과 값 리턴
+        if(row >= land_row_size-1){
+            return ret;
+        }
+        int val = 0;
+        for(int col_idx = 0; col_idx < 4; col_idx++){
+            if (col_idx != col){
+                int r = DFS(land, row + 1, col_idx,land_row_size);
+                if(r > val){
+                    val = r;
+                }
+            }
+        }
+        ret += val;
+        return ret;
+    }
+}
+*/
